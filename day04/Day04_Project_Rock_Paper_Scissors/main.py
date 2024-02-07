7b10dd954ea4b10a39d7c9ed4244a1de97845f4b
Nadir <nadirsaleh@gmail.com>
2024-02-07 15:55:22 +1000

    Updated Rock Paper Scissors Project

--- day04/Day04_Project_Rock_Paper_Scissors/main.py
+++ day04/Day04_Project_Rock_Paper_Scissors/main.py
@@ -46,62 +46,42 @@ choice = [rock, paper, scissors]
 score = 0
 ai_score = 0
 ai_visual = ""
+player_code = 0
+
 
 while True:
     clear_screen()
     player_choice = input("Choose (r)ock, (p)aper or (s)cissors: ").lower()
-    ch_ai = ""
     ai_choice = randint(0, len(choice) - 1)
-    if ai_choice == 0:
-        ch_ai = "r"
-        ai_visual = choice[0]
-    elif ai_choice == 1:
-        ch_ai = "p"
-        ai_visual = choice[1]
-    else:
-        ch_ai = "s"
-        ai_visual = choice[2]
+    ai_visual = choice[ai_choice]
 
     if player_choice == "r":
         print(f"You played rock...\n{rock}")
-
+        player_code = 0
     elif player_choice == "p":
         print(f"You played paper...\n{paper}")
-
+        player_code = 1
     elif player_choice == "s":
         print(f"You played paper...\n{scissors}")
-
+        player_code = 2
     else:
         print("invalid option!")
         continue
-
-    print(f"AI played:\n{ai_visual}")
-    if ch_ai == player_choice:
-        print("Tie!")
-
-    if player_choice == "r":
-        if ch_ai == "s":
-            print("You gain a point.")
-            score += 1
-        elif ch_ai == "p":
-            print("AI gains a point.")
-            ai_score += 1
-
-    if player_choice == "p":
-        if ch_ai == "r":
-            print("You gain a point.")
-            score += 1
-        if ch_ai == "s":
-            print("AI gains a point.")
-            ai_score += 1
-
-    if player_choice == "s":
-        if ch_ai == "r":
-            print("AI gains a point.")
-            ai_score += 1
-        elif ch_ai == "p":
-            print("You gain a point.")
-            score += 1
+    print(f"AI:\n{ai_visual}")
+    if player_code == 0 and ai_choice == 2:
+        print("You gain a point.")
+        score += 1
+    elif ai_choice == 0 and player_code == 2:
+        print("AI gains a point.")
+        ai_score += 1
+    elif ai_choice > player_code:
+        print("AI gains a point.")
+        ai_score += 1
+    elif player_code > ai_choice:
+        print("You gain a point.")
+        score += 1
+    elif ai_choice == player_code:
+        print("Tie.")
 
     print(f"You: {score} AI: {ai_score}")
     input("Press Enter to continue. ")
